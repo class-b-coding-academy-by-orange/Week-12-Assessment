eb0f775e8ec956140e595ebb74821dcf74ff88b6
abd.maqableh <51744473+abd-maqableh@users.noreply.github.com>
2019-09-01 10:44:53 +0300

    Finish The Assessment

--- client/src/App.js
+++ client/src/App.js
@@ -29,10 +29,11 @@ export default class App extends Component {
   //Q1: we have 4 errors here please fix them
   addTodoItem = (newTask, cb) => {
     axios
-      .get('http://localhost:9000/task', newTask)
+      .post('http://localhost:9000/tasks', newTask)
       .then(res => {
-        const tasks = res;
-        this.state.task = tasks;
+        const tasks = res.data;
+
+        this.setState({tasks:[...this.state.tasks,tasks]});
       })
       .catch(error => {
         console.log(error);
