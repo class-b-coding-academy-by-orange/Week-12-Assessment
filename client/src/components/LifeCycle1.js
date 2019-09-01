2777a81fa7c334764745365ec9bcc7d1e93f68f3
OsaidAyadi <osaid.step@gmail.com>
2019-09-01 10:31:46 +0300

    reg

--- client/src/components/LifeCycle1.js
+++ client/src/components/LifeCycle1.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 
 export class LifeCycle1 extends Component {
   constructor() {
@@ -8,9 +10,13 @@ export class LifeCycle1 extends Component {
     };
   }
 
-  changeState = () => {
+  changeState=()=>
+ {
     this.setState({ title: 'dont update the title' });
   };
+
+ 
+  
 
   /*
     Q4: in this component you should didnt update:
@@ -21,22 +27,35 @@ export class LifeCycle1 extends Component {
   */
 
   render() {
+
+    const {message,title} = this.props;
+
+
+    
     return (
       <div style={{ border: 'solid 2px red' }}>
         <h6>LifeCycle 1 Component</h6>
         <button onClick={this.changeState}>Change LifeCycle state</button>
         <p>STATE TITLE: {this.state.title}</p>
-        <p>PROPS MESSAGE: {this.props.message}</p>
-        <p>PROPS TITLE: {this.props.title}</p>
+        <p>PROPS MESSAGE: {message}</p>
+        <p>PROPS TITLE: {title}</p>
       </div>
     );
   }
 }
 
-export default LifeCycle1;
 
+
+
+
+export default LifeCycle1;
+LifeCycle1.propTypes = {
+  name: PropTypes.string,
+  title: PropTypes.string.isRequired
+};
 /*
   Q5: in this component you should use props type:
   1- the props message should be a string
   2- the props title should be a string and require
 */
+
